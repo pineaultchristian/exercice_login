3ee62ca73a7778479889fcc1af7b64ed2126ed8a
martinbeaudry@hotmail.com <vagrant@zend.localdomain.local>
2015-11-03 22:35:11 -0500

    Changes text type for password type

--- homeLogin.php
+++ homeLogin.php
@@ -4,7 +4,7 @@
 
 <form action="middleLogin.php" method="post">
 Username: <input type="text" name="username"><br>
-Password: <input type="text" name="password"><br>
+Password: <input type="password" name="password"><br>
 <input type="submit">
 </form>
 
